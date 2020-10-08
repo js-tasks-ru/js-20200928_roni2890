04c8db9a2ac14e9f3edad1bbfae6ac2873eb02d7
VitalyKolmogorov <Roni2890@yandex.ru>
2020-10-08 18:03:05 +0300

    задание 2. Реализована функция возврата объект с нужными свойствами

--- 02-javascript-data-types/2-pick/index.js
+++ 02-javascript-data-types/2-pick/index.js
@@ -5,5 +5,10 @@
  * @returns {object} - returns the new object
  */
 export const pick = (obj, ...fields) => {
-
+  const  tempMap = new Map(Object.entries(obj));
+  //у нас есть список методов и список то что надо, если входит в список то оставляем иначе удаляем
+  tempMap.forEach((value, key) => {if (!fields.includes(key)){
+    tempMap.delete(key);
+  }})
+  return Object.fromEntries(tempMap);
 };
