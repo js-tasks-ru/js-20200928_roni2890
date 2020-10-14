a42d8da8a4e10da904e9237f1dd5210b58831476
VitalyKolmogorov <Roni2890@yandex.ru>
2020-10-14 10:39:40 +0300

    3 задание. Создание геттера, который возвращает нужное свойство

--- 03-objects-arrays-intro-to-testing/1-create-getter/index.js
+++ 03-objects-arrays-intro-to-testing/1-create-getter/index.js
@@ -4,5 +4,17 @@
  * @returns {function} - function-getter which allow get value from object by set path
  */
 export function createGetter(path) {
-
+//разобрать на составляющие
+  const parsedPath = path.split('.');
+//в цикле найти нужное свойство
+  return function(obj) {
+    let innerObj = obj;
+    for (const property of parsedPath) {
+      if (innerObj === undefined){//А можно ли так писать? if (!innerObj) !{}=false !NaN=true или лучше явно сравнивать
+        break;
+      }
+      innerObj = innerObj[property];
+    }
+    return innerObj;
+  };
 }
